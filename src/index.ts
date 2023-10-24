e5717c3011c905f8f62f95d9f30442de3632dc3c
unknown <you@example.com>
2023-10-24 22:07:57 +0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { fromFetch } from 'rxjs/fetch';
+import { switchMap, of, catchError } from 'rxjs';
+
+const data$ = fromFetch(
+  'https://api.github.com/search/repositories?q=rxjs'
+).pipe(
+  switchMap((response) => {
+    if (response.ok) {
+      // OK return data
+      return response.json();
+    } else {
+      // Server is returning a status requiring the client to try something else.
+      return of({ error: true, message: `Error ${response.status}` });
+    }
+  }),
+  catchError((err) => {
+    // Network or other error, handle appropriately
+    console.error(err);
+    return of({ error: true, message: err.message });
+  })
+);
+
+data$.subscribe({
+  next: (result) => console.log(result),
+  complete: () => console.log('done'),
+});
